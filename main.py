363989d090c1cc0faa2e58668aa6f1a91dcd19bc
Daniel Cronin <daniel@dcron.in>
2020-01-05 21:05:31 -0700

    added some methods to say module

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 from fileread import *
 from say import Say
+
 if __name__ == '__main__':
     file = FileRead(sys.argv[1])
+
     for command in file.COMMANDS:
         if 'say' in command:
-
             Say(command)
--- say.py
+++ say.py
@@ -1,5 +1,33 @@
 class Say:
 
     def __init__(self, string):
-        self.STRING = string
-        print(self.STRING)
+        self.RAW_STRING = string
+        self.ARG = ''
+        self.parse_command()
+
+    def parse_command(self):
+        command_line = self.RAW_STRING
+
+        if 'say' not in command_line:
+            raise IncorrectSayCommand('No "say" keyword found in line.')
+        else:
+            self.ARG = command_line.split('say: ')[1]
+            self.determine_command()
+
+    def determine_command(self):
+        if self.ARG == '"':
+            self.display_string()
+        elif '+' or '-' or '/' or '*' in self.ARG:
+            self.display_string()
+
+    def display_string(self):
+        the_string = self.ARG[1:-1]
+        print(the_string)
+
+    def display_math(self):
+        # TODO: create method to process math arguments
+        pass
+
+
+class IncorrectSayCommand(Exception):
+    pass
